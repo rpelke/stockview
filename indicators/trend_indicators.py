6d39214c8e63698b2b5c8859af670b525feade23
Rebecca Pelke <pelke@ice.rwth-aachen.de>
2024-01-07 20:02:18 +0100

    Plot ADX14, calculate crossovers
    Signed-off-by: Rebecca Pelke <pelke@ice.rwth-aachen.de>

--- indicators/trend_indicators.py
+++ indicators/trend_indicators.py
@@ -1,9 +1,12 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 
 
 def add_adx(df: pd.core.frame.DataFrame, n_smooth: int = 14) :
-    """
+    """The Average Directional Index (ADX) signals market direction, trend presence, and momentum.
+    +DI higher suggests an upward trend, while a greater -DI indicates a downward trend.
+    ADX values above 20 confirm a trend.
+    We currently use an EMA over 14 trading days.
     """
     assert n_smooth == 14, f'Not implemented.'
     assert len(df) > 2*n_smooth+2, f'At least {2*n_smooth+2} trading days for ADX{n_smooth} needed.'
@@ -11,8 +14,8 @@ def add_adx(df: pd.core.frame.DataFrame, n_smooth: int = 14) :
     # UpMove = Curr High - Prev High
     # DownMove = Prev Low - Curr Low
     temp_df = pd.DataFrame(index=df.index)
-    temp_df['UpMove'] = df['High'] - df.shift(1)['High']
-    temp_df['DownMove'] = df.shift(1)['Low'] - df['Low']
+    temp_df['UpMove'] = df['High'] - df['High'].shift(1)
+    temp_df['DownMove'] = df['Low'].shift(1) - df['Low']
 
     # DMplus = UpMove if UpMove > DownMove and UpMove > 0, else: DMplus = 0
     # DMminus = DownMove if DownMove > Upmove and Downmove > 0, else DMminus = 0
@@ -21,8 +24,8 @@ def add_adx(df: pd.core.frame.DataFrame, n_smooth: int = 14) :
 
     # Calculate True Range (TR)
     temp_df['Th_Tl'] = df['High'] - df['Low']
-    temp_df['Th_Yc'] = df['High'] - df.shift(1)['Close']
-    temp_df['Tl_Yc'] = df['Low'] - df.shift(1)['Close']
+    temp_df['Th_Yc'] = df['High'] - df['Close'].shift(1)
+    temp_df['Tl_Yc'] = df['Low'] - df['Close'].shift(1)
     df['TR1'] = temp_df.iloc[1:].apply(lambda row: max(row['Th_Tl'], abs(row['Th_Yc']), abs(row['Tl_Yc'])), axis=1)
     
     # First TR14: Sum of first 14 TR1
@@ -44,8 +47,7 @@ def add_adx(df: pd.core.frame.DataFrame, n_smooth: int = 14) :
     df['DM14minus'] = dm14minus
     df['DM14plus'] = dm14plus
 
-    # Calculate DI14plus and DI14minus
-    # DI14minus = DM14minus/TR14, DI14plus = DM14plus/TR14
+    # Calculate Directional Indicators (DI) +DI (DI14plus) and -DI (DI14minus)
     df['DI14minus'] = 100 * df['DM14minus'] / df['TR14']
     df['DI14plus'] = 100 * df['DM14plus'] / df['TR14']
 
@@ -60,3 +62,41 @@ def add_adx(df: pd.core.frame.DataFrame, n_smooth: int = 14) :
     for i in range(29, len(df)):
         adx[i] = (13 * adx[i-1] + df.iloc[i]['DX']) / 14
     df[f'ADX{n_smooth}'] = adx
+
+
+def plot_adx(path: str, df: pd.core.frame.DataFrame, company: str, adx_num: int = 14, week_trend: int = 20, strong_trend: int = 25) :
+    """Plot stock value, ADX, +DI, and -DI for one company.
+    Week trend: strong_trend > ADX > week_trend.
+    Strong trend: ADX > strong_trend. Otherwise: No trend.
+    Crossovers of the -DI and +DI lines can be used to generate trade signals.
+    For example, if the +DI line crosses above the -DI line and the ADX is above 20, or ideally above 25, then that is a potential signal to buy.
+    On the other hand, if the -DI crosses above the +DI, and the ADX is above 20 or 25, then that is an opportunity to enter a potential short trade.
+    """
+    tmp_df = pd.DataFrame(index=df.index)
+    tmp_df['diff'] = df.apply(lambda row: 1 if row[f'DI{adx_num}plus'] > row[f'DI{adx_num}minus'] else 0, axis=1)
+    tmp_df['crossover'] = tmp_df['diff'] - tmp_df['diff'].shift(1)
+
+    fig, axs = plt.subplots(2, figsize=(20,5))
+    fig.suptitle(f'ADX{adx_num} for Company {company}')
+    for day in list(tmp_df.loc[tmp_df['crossover'] == 1].index) :
+        if df.loc[day][f'ADX{adx_num}'] > strong_trend :
+            axs[0].axvline(x=day, color='green', linestyle='--', linewidth=0.5)
+            axs[1].axvline(x=day, color='green', linestyle='--', linewidth=0.5)
+    for day in list(tmp_df.loc[tmp_df['crossover'] == -1].index) :
+        if df.loc[day][f'ADX{adx_num}'] > strong_trend :
+            axs[0].axvline(x=day, color='red', linestyle='--', linewidth=0.5)
+            axs[1].axvline(x=day, color='red', linestyle='--', linewidth=0.5)
+
+    axs[0].plot(df.index, df['High'], label=f'{company} High', color='blue', linewidth=1)
+
+    axs[1].axhline(y=week_trend, color='grey', linestyle='--', linewidth=0.5)
+    axs[1].axhline(y=strong_trend, color='grey', linestyle='--', linewidth=0.5)
+    axs[1].plot(df.index, df[f'DI{adx_num}plus'], label=f'+DI{adx_num}', color='green', linewidth=0.5)
+    axs[1].plot(df.index, df[f'DI{adx_num}minus'], label=f'-DI{adx_num}', color='red', linewidth=0.5)
+    axs[1].plot(df.index, df[f'ADX{adx_num}'], label=f'ADX{adx_num}', color='blue', linewidth=0.5)
+
+    axs[0].set_xlim(left=df.index.min(), right=df.index.max())
+    axs[1].set_xlim(left=df.index.min(), right=df.index.max())
+
+    plt.legend()
+    plt.savefig(f'{path}/ADX{adx_num}_{company}.pdf')
--- stockview.py
+++ stockview.py
@@ -1,15 +1,9 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
-import numpy as np
+import os
 
 from common.get_data import *
 from indicators.moving_average import *
 from indicators.trend_indicators import *
 
-vw = yf.Ticker('VWAGY')
-vw_hist = vw.history(period='4y')
-tesla = yf.Ticker('TSLA')
-tesla_hist = tesla.history(period='4y')
 
 df = get_last_years(years=4, company='VWAGY')
 
@@ -19,20 +13,4 @@ add_ema(df=df, n_smooth=10)
 add_wma(df=df, window_size=5)
 add_adx(df=df, n_smooth=14)
 
-
-### Plot
-fig, ax1 = plt.subplots(figsize=(20,5))
-ax2 = ax1.twinx()
-ax1.plot(vw_hist.index, vw_hist["High"], label="VW High", color="blue", linewidth=0.5)
-ax2.plot(tesla_hist.index, tesla_hist["High"], label="Tesla High", color="green", linewidth=0.5)
-fig.legend()
-fig.savefig("stock_hist_high.png")
-
-fig, ax1 = plt.subplots(figsize=(20,5))
-ax2 = ax1.twinx()
-ax1.plot(vw_hist.index[1:], np.diff(vw_hist["High"]), label="VW High Diff", color="darkblue", linewidth=0.5)
-ax2.plot(tesla_hist.index[1:], np.diff(tesla_hist["High"]), label="Tesla High Diff", color="darkgreen", linewidth=0.5)
-ax1.axhline(y=0, color="darkblue")
-ax2.axhline(y=0, color="darkgreen")
-fig.legend()
-fig.savefig("diff_hist_high.png")
+plot_adx(path=f'{os.getcwd()}', df=df, company='VW', adx_num=14, strong_trend=25)
